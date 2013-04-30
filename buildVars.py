cc1ec96b2947898a38c1347e8bdccd2caa705709
James Teh <jamie@jantrid.net>
2013-04-30 22:08:53 +1000

    Customise add-on build. Remove manifest.ini files, since these will be auto-generated.

--- buildVars.py
+++ buildVars.py
@@ -7,28 +7,29 @@ _ = lambda x : x
 # Add-on information variables
 addon_info = {
 	# add-on Name
-	"addon-name" : "addon-template",
-	# Add-on description
+	"addon-name" : "ocr",
+	# Add-on summary
 	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
-	"addon-summary" : _("Add-on quick summary"),
+	"addon-summary" : _("OCR"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on installation and add-on information
-	"addon-description" : _("""Description for the add-on.
-It can span multiple lines."""),
+	"addon-description" : _("""Performs optical character recognition (OCR) to extract text from an object which is inaccessible.
+The Tesseract OCR engine is used.
+To perform OCR, move to the object in question using object navigation and press NVDA+r.
+You can set the OCR recognition language by going to the NVDA preferences menu and selecting OCR settings."""),
 	# version
-	"addon-version" : "x.y.z",
+	"addon-version" : "2013.1",
 	# Author(s)
-	"addon-author" : "name <name@domain.com>",
+	"addon-author" : "NV Access Limited & other contributors",
 	# URL for the add-on documentation support
 	"addon-url" : None
 }
-
 
 import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = []
+pythonSources = ["addon/globalPlugins/ocr/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
