5125d75eadb42904a57270e59413e0a9a40b190f
James Teh <jamie@jantrid.net>
2013-09-06 15:55:34 +1000

    deps.py: Don't include Tesseract languages, since the add-on is just too large if we include them all.

--- deps.py
+++ deps.py
@@ -7,11 +7,8 @@ import urllib
 import fnmatch
 import shutil
 import zipfile
-import tarfile
 
-# Sometimes, different packages have different versions.
-TESSERACT_VERSIONS = ("3.02.02", "3.02")
-TESSERACT_DOWNLOAD_URL = "http://code.google.com/p/tesseract-ocr/downloads/list"
+TESSERACT_DOWNLOAD_URL = "http://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02-win32-portable.zip"
 PIL_DOWNLOAD_URL = "http://effbot.org/downloads/PIL-1.1.7.win32-py2.7.exe"
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -20,28 +17,13 @@ PLUGIN_DIR = os.path.join(ROOT_DIR, "addon", "globalPlugins", "ocr")
 
 depFiles = set()
 
-def getTesseractUrls():
-	data = urllib.urlopen(TESSERACT_DOWNLOAD_URL).read()
-	versionRe = "(?:%s)" % "|".join(re.escape(ver) for ver in TESSERACT_VERSIONS)
-	for m in re.finditer(
-		r'href="(.*?tesseract-ocr-%s(?:'
-			r'-win32-portable\.zip|'
-			# Language data
-			r'\.[a-z]{3}(?:_[a-z]{3})?\.tar\.gz'
-			r'))"' % versionRe,
-		data
-	):
-		# Google Code doesn't seem to include the http: protocol prefix.
-		yield "http:" + m.group(1)
-
 def downloadDeps():
 	try:
 		os.mkdir(DEPS_DIR)
 	except OSError:
 		pass
 
-	urls = list(getTesseractUrls())
-	urls.append(PIL_DOWNLOAD_URL)
+	urls = [TESSERACT_DOWNLOAD_URL, PIL_DOWNLOAD_URL]
 
 	print("Downloading dependencies")
 	for url in urls:
@@ -85,22 +67,6 @@ def extractTesseract():
 			with zf.open(realFn) as inf, file(extractFn, "wb") as outf:
 				shutil.copyfileobj(inf, outf)
 
-def extractTesseractLangs():
-	print "Extracting Tesseract languages"
-	dataDir = os.path.join(PLUGIN_DIR, "tesseract", "tessdata")
-	for tfn in depFiles:
-		if not fnmatch.fnmatch(tfn, "*/tesseract-ocr-*.*.tar.gz"):
-			continue
-		with tarfile.open(tfn) as tf:
-			for info in tf:
-				if info.isdir():
-					continue
-				fn = os.path.basename(info.name)
-				extractFn = os.path.join(dataDir, fn)
-				inf = tf.extractfile(info)
-				outf = file(extractFn, "wb")
-				shutil.copyfileobj(inf, outf)
-
 def extractPil():
 	pilDir = os.path.join(PLUGIN_DIR, "PIL")
 	print "Extracting PIL"
@@ -122,7 +88,6 @@ def extractPil():
 def main():
 	downloadDeps()
 	extractTesseract()
-	extractTesseractLangs()
 	extractPil()
 
 if __name__ == "__main__":
